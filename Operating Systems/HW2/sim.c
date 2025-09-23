9966248af87ebba529777c400821b1cd2f2ff77e
toki <clevertrever31@gmail.com>
2025-09-23 10:30:23 -0500

    Basic sim.c setup, just adding methods

---
+++ Operating Systems/HW2/sim.c
@@ -0,0 +1,33 @@
+/*********************************************************************
+* Author: Trever J. Fuhrer, September 2025
+* sim: simple simulator for scheduling algorithms.
+* Usage: 
+**********************************************************************/
+
+#include "utils.h"
+
+/**
+ * Description: 
+ *  - Runs the First-Come-First-Serve (FCFS) scheduling algorithm.
+ * 
+ * Parameters:
+ *  - argc: Argument count from command line.
+ *  - argv: Argument vector from command line.
+ */
+void run_fcfs(int argc, char *argv[]) {
+    printf("Running FCFS scheduling algorithm...\n");
+
+}
+
+/**
+ * Description: 
+ *  - Runs the Round-Robin (RR) scheduling algorithm.
+ * 
+ * Parameters:
+ *  - argc: Argument count from command line.
+ *  - argv: Argument vector from command line.
+ */
+void run_rr(int argc, char *argv[]) {
+    printf("Running RR scheduling algorithm...\n");
+    
+}
