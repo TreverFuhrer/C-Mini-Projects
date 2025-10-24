e8789d5ae0972e1bf39f13ea8bca233951875897
toki <clevertrever31@gmail.com>
2025-10-24 15:30:34 -0500

    Created base functions and documentation

---
+++ Operating Systems/HW3/pager.c
@@ -0,0 +1,29 @@
+/*********************************************************************
+* Trever Fuhrer, Oct 2025
+* Virtual Memory Simulator
+**********************************************************************/
+#include "utils.h"
+
+/**
+ * Description:
+ *  - Runs the First-In-First-Out (FIFO) page replacement algorithm.
+ *
+ * Parameters:
+ *  - argc: Argument count from command line.
+ *  - argv: Argument vector from command line.
+ */
+void fifo(int argc, char *argv[]) {
+    
+}
+
+/**
+ * Description:
+ *  - Runs the Least Recently Used (LRU) page replacement algorithm.
+ *
+ * Parameters:
+ *  - argc: Argument count from command line.
+ *  - argv: Argument vector from command line.
+ */
+void lru(int argc, char *argv[]) {
+
+}
