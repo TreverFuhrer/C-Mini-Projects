30267cc7d0bf792a4dbdb8b489cf534e31c0984b
toki <clevertrever31@gmail.com>
2025-10-24 16:26:53 -0500

    Correct utils.h

--- Operating Systems/HW3/utils.h
+++ Operating Systems/HW3/utils.h
@@ -4,27 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <time.h>
 
-#define MAX_NAME_LEN 20
-#define MAX_PROCESSES 5
+#define MAX_LINE 128
+#define PAGE_SIZE 4096 
 
-// Process structure
-typedef struct {
-    char pid[MAX_NAME_LEN];        // process ID
-    int arrival_time;               // Arrival time (ms)
-    int run_time;                   // Total CPU time (ms) needed
-    int remaining_time;             // Remaining execution time (for RR)
-    int initial_schedule_time;      // Initial schedule time by CPU [You will need to keep track of it for response time calculation]
-    int completion_time;            // Completion time of a process [You will need to keep track of it for tunraround time calculation]
-} Process;
-
-
-// Function Prototypes
-void run_fcfs(int argc, char *argv[]);
-void run_rr(int argc, char *argv[]);
-
+void fifo(char *filename, int frame_count);
+void lru(char *filename, int frame_count);
 void usage(char *str); // already implemented in tester.c
 
 #endif
